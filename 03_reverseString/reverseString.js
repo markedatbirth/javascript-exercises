2ef7e36518afef2624dbdf62a7ffb42a8d7066d3
“Kyle <128731127+markedatbirth@users.noreply.github.com>
2023-06-11 16:33:46 -0400

    update JS exercise completion

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+// My Solution - With assistance from chatGPT prior to knowing TOP frowns upon this. 
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+
+//     let splitString = str.split("");
+//     let reverseArray = splitString.reverse();
+//     let joinArray = reverseArray.join("");
+//     return joinArray;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,44 @@
-const sumAll = function() {
+// My Solution
 
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (typeof num2 !== "number") {
+        return 'ERROR'; 
+    } else if (num2 > num1) {
+        for (let i = 0; i <= num2; i++) {
+            sum += i;
+        }
+    } else if (num1 > num2) {
+        for (let i = 0; i <= num1; i++) {
+            sum += i;
+        }
+    } else { 
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*
+
+Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
+
+```javascript
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+```
+
+
+## Hints
+
+Think about how you would do this on pen and paper and then how you might translate that process into code:
+- make sure you pay attention to the function parameters
+- create a variable to hold the final sum
+- loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
+- on each iteration add the number to the sum
+- return the sum after finishing the loop
+
+*/
